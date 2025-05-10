722c3ed8e87581e118dba7ca72a7a9154ed0762e
darshanramani <darshanramani36@gmail.com>
2025-05-10 18:15:15 +0530

    try fail test case

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,7 +1,7 @@
 from src.math_operation import add,sub 
 
 def test_add():
-    assert add(3,6)==9
+    assert add(3,6)==10
     assert add(-8,-8)==-16
     assert add(0,0)==0
     assert add(6,5)==11
@@ -11,4 +11,3 @@ def test_sub():
     assert sub(-8,-6)==-2
 
 
-    
